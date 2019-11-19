c0d8cfe21b89b096e37aae8bc5008d6f07523b54
Bob Bloom <bob.bloom@lasallesoftware.ca>
2019-11-19 13:34:23 -0500

    Create the failed_jobs table, as part of the conversion to Laravel 6. #27

---
+++ database/migrations/2019_01_29_183255_create_failed_jobs_table.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the Lasalle Software library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright  (c) 2019 The South LaSalle Trading Corporation
+ * @license    http://opensource.org/licenses/MIT MIT
+ * @author     Bob Bloom
+ * @email      bob.bloom@lasallesoftware.ca
+ * @link       https://lasallesoftware.ca
+ * @link       https://packagist.org/packages/lasallesoftware/lsv2-library-pkg
+ * @link       https://github.com/LaSalleSoftware/lsv2-library-pkg
+ *
+ */
+
+// LaSalle Software
+use Lasallesoftware\Library\Database\Migrations\BaseMigration;
+
+// Laravel classes
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateFailedJobsTable extends BaseMigration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
+        });
+    }
+}
