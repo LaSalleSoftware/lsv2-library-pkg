2390030fbb1bde7425e0897761c42637b9dc070e
Bob Bloom <bob.bloom@lasallesoftware.ca>
2020-01-15 23:21:57 -0500

    Update LasalleinstallenvCommand.php with front-end env var params. #40

--- src/Commands/LasalleinstallenvCommand.php
+++ src/Commands/LasalleinstallenvCommand.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the Lasalle Software library
+ * This file is part of the Lasalle Software library.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,29 +14,25 @@
  * @license    http://opensource.org/licenses/MIT MIT
  * @author     Bob Bloom
  * @email      bob.bloom@lasallesoftware.ca
- * @link       https://lasallesoftware.ca
- * @link       https://packagist.org/packages/lasallesoftware/lsv2-library-pkg
- * @link       https://github.com/LaSalleSoftware/lsv2-library-pkg
  *
+ * @see       https://lasallesoftware.ca
+ * @see       https://packagist.org/packages/lasallesoftware/lsv2-library-pkg
+ * @see       https://github.com/LaSalleSoftware/lsv2-library-pkg
  */
 
 namespace Lasallesoftware\Library\Commands;
 
 // LaSalle Software class
+use Illuminate\Console\ConfirmableTrait;
+// Laravel classes
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
 use Lasallesoftware\Library\Common\Commands\CommonCommand;
 
-// Laravel classes
-use http\Exception\BadQueryStringException;
-use Illuminate\Console\ConfirmableTrait;
-use Symfony\Component\Console\Input\InputOption;
-use Illuminate\Filesystem\Filesystem;
-
 /**
- * Class LasalleinstallpartoneCommand
+ * Class LasalleinstallpartoneCommand.
  *
  * First of two artisan command installation scripts
- *
- * @package Lasallesoftware\Library\Commands\LasalleinstallpartoneCommand
  */
 class LasalleinstallenvCommand extends CommonCommand
 {
@@ -58,10 +54,7 @@ class LasalleinstallenvCommand extends CommonCommand
 
     /**
      * Create a new config command instance.
-     *
-     * @param  \Illuminate\Filesystem\Filesystem  $files
-     * @return void
-*/
+     */
     public function __construct(Filesystem $files)
     {
         parent::__construct();
@@ -69,8 +62,6 @@ class LasalleinstallenvCommand extends CommonCommand
 
     /**
      * Execute the console command.
-     *
-     * @return void
      */
     public function handle()
     {
@@ -86,9 +77,9 @@ class LasalleinstallenvCommand extends CommonCommand
         $this->line('                       Welcome to my LaSalle Software\'s');
         $this->line('               Environment Variables Installation Artisan Command!');
         $this->line('--------------------------------------------------------------------------------');
-        $this->line('  You are installing the ' . mb_strtoupper(env('LASALLE_APP_NAME')) . ' LaSalle Software Application.');
+        $this->line('  You are installing the '.mb_strtoupper(env('LASALLE_APP_NAME')).' LaSalle Software Application.');
         echo "\n";
-        $this->line('  You are installing to your ' . $this->getLaravel()->environment() . ' environment.');
+        $this->line('  You are installing to your '.$this->getLaravel()->environment().' environment.');
         $this->line('--------------------------------------------------------------------------------');
         $this->line('  This command sets some variables in your .env.');
         $this->line('--------------------------------------------------------------------------------');
@@ -110,8 +101,6 @@ class LasalleinstallenvCommand extends CommonCommand
         // END: INTRO
         // -------------------------------------------------------------------------------------------------------------
 
-
-
         // -------------------------------------------------------------------------------------------------------------
         // START: ARE YOU SURE YOU WANT TO RUN THIS COMMAND?
         echo "\n\n\n";
@@ -121,16 +110,15 @@ class LasalleinstallenvCommand extends CommonCommand
         echo "\n";
         $this->alert('Are you sure that you want to run this command?');
         $runConfirmation = $this->ask('<fg=yellow>(type the word "yes" to continue)</>');
-        if ($runConfirmation != strtolower("yes")) {
+        if ($runConfirmation != strtolower('yes')) {
             $this->line('<fg=red;bg=yellow>OK, you did not type "yes", so I am NOT going to continue running this command. Bye!</>');
             $this->echoOutro();
+
             return;
         }
         $this->comment('ok... you said that you want to continue running this command. Let us continue then...');
         // END: ARE YOU SURE YOU WANT TO RUN THIS COMMAND?
         // -------------------------------------------------------------------------------------------------------------
-
-
 
         // -------------------------------------------------------------------------------------------------------------
         // START: SET THE PARAMS IN .ENV
@@ -145,10 +133,10 @@ class LasalleinstallenvCommand extends CommonCommand
         $this->comment('An example is: LaSalle Software Administration App');
         $this->comment(' ');
         $appName = $this->ask('(I do *not* check for syntax or for anything, so please type c-a-r-e-f-u-l-l-y!)');
-        $this->comment('You typed "' . $appName . '".');
-        $this->comment('Attempting to set APP_NAME in your .env to "'. $appName . '"...');
+        $this->comment('You typed "'.$appName.'".');
+        $this->comment('Attempting to set APP_NAME in your .env to "'.$appName.'"...');
         $this->writeEnvironmentFileWithNewKey('DummyAppName', $appName, true);
-        $this->info("Attempt to modify your env's APP_NAME to " . $appName . " is finished!");
+        $this->info("Attempt to modify your env's APP_NAME to ".$appName.' is finished!');
 
         // SET APP_URL
         echo "\n\n";
@@ -163,9 +151,9 @@ class LasalleinstallenvCommand extends CommonCommand
         $this->comment('  * example: https://lasallesoftware.ca:8888');
         $this->comment(' ');
         $appURL = $this->ask('(I do *not* check for syntax or for anything, so please type c-a-r-e-f-u-l-l-y!)');
-        $this->comment('Attempting to set APP_URL in your .env to "'. $appURL . '"...');
+        $this->comment('Attempting to set APP_URL in your .env to "'.$appURL.'"...');
         $this->writeEnvironmentFileWithNewKey('DummyAppURL', $appURL, false);
-        $this->info("Attempt to modify your env's APP_URL to " . $appURL . " is finished!");
+        $this->info("Attempt to modify your env's APP_URL to ".$appURL.' is finished!');
 
         // SET LASALLE_APP_DOMAIN_NAME
         echo "\n\n";
@@ -173,16 +161,15 @@ class LasalleinstallenvCommand extends CommonCommand
         $this->line('  Now setting your LASALLE_APP_DOMAIN_NAME environment variable');
         $this->line('-----------------------------------------------------------------------');
         echo "\n\n";
-        $this->comment("This is done automatically based on your APP_URL.");
+        $this->comment('This is done automatically based on your APP_URL.');
 
         $lasalleAppDomainName = $this->getLasalleAppDomainName($appURL);
         $this->comment('Attempting to set LASALLE_APP_DOMAIN_NAME in your .env to "'.$lasalleAppDomainName.'""...');
         $this->writeEnvironmentFileWithNewKey('DummyLasalleAppDomainName', $lasalleAppDomainName, false);
-        $this->info("Attempt to modify your env's LASALLE_APP_DOMAIN_NAME to " . $lasalleAppDomainName . " is finished!");
-
+        $this->info("Attempt to modify your env's LASALLE_APP_DOMAIN_NAME to ".$lasalleAppDomainName.' is finished!');
 
         // SET DB_DATABASE
-        if (env('LASALLE_APP_NAME') == 'adminbackendapp') {
+        if ('adminbackendapp' == env('LASALLE_APP_NAME')) {
             echo "\n\n";
             $this->line('-----------------------------------------------------------------------');
             $this->line('  Now setting your DB_DATABASE environment variable');
@@ -192,13 +179,13 @@ class LasalleinstallenvCommand extends CommonCommand
             $this->comment('Are you installing with Forge? Then Forge, likely, already updated this');
             $this->comment('environment variable, so just hit ENTER.');
             $appDbDatabase = $this->ask('(What is the name of your database?)');
-            $this->comment('Attempting to set DB_DATABASE in your .env to "'. $appDbDatabase . '"...');
+            $this->comment('Attempting to set DB_DATABASE in your .env to "'.$appDbDatabase.'"...');
             $this->writeEnvironmentFileWithNewKey('DummyDbDatabase', $appDbDatabase, false);
-            $this->info("Attempt to modify your env's DB_DATABASE to " . $appDbDatabase . " is finished!");
+            $this->info("Attempt to modify your env's DB_DATABASE to ".$appDbDatabase.' is finished!');
         }
 
         // SET DB_USERNAME
-        if (env('LASALLE_APP_NAME') == 'adminbackendapp') {
+        if ('adminbackendapp' == env('LASALLE_APP_NAME')) {
             echo "\n\n";
             $this->line('-----------------------------------------------------------------------');
             $this->line('  Now setting your DB_USERNAME environment variable');
@@ -208,13 +195,13 @@ class LasalleinstallenvCommand extends CommonCommand
             $this->comment('Are you installing with Forge? Then Forge, likely, has already updated');
             $this->comment('this environment variable, so just hit ENTER.');
             $appDbUsername = $this->ask("What is the name of your database's user?");
-            $this->comment('Attempting to set DB_USERNAME in your .env to "'. $appDbUsername . '"...');
+            $this->comment('Attempting to set DB_USERNAME in your .env to "'.$appDbUsername.'"...');
             $this->writeEnvironmentFileWithNewKey('DummyDbUsername', $appDbUsername, false);
-            $this->info("Attempt to modify your env's DB_USERNAME to " . $appDbUsername . " is finished!");
+            $this->info("Attempt to modify your env's DB_USERNAME to ".$appDbUsername.' is finished!');
         }
 
         // SET DB_PASSWSORD
-        if (env('LASALLE_APP_NAME') == 'adminbackendapp') {
+        if ('adminbackendapp' == env('LASALLE_APP_NAME')) {
             echo "\n\n";
             $this->line('-----------------------------------------------------------------------');
             $this->line('  Now setting your DB_PASSWORD environment variable');
@@ -224,14 +211,26 @@ class LasalleinstallenvCommand extends CommonCommand
             $this->comment('Are you installing with Forge? Then Forge, likely, has already updated');
             $this->comment('this environment variable, so just hit ENTER and then check your .env in Forge.');
             $appDbPassword = $this->ask("What is your database's password?");
-            $this->comment('Attempting to set DB_PASSWORD in your .env to "'. $appDbPassword . '"...');
+            $this->comment('Attempting to set DB_PASSWORD in your .env to "'.$appDbPassword.'"...');
             $this->writeEnvironmentFileWithNewKey('DummyDbPassword', $appDbPassword, false);
-            $this->info("Attempt to modify your env's DB_PASSWORD to " . $appDbPassword . " is finished!");
+            $this->info("Attempt to modify your env's DB_PASSWORD to ".$appDbPassword.' is finished!');
+        }
+
+        // LASALLE_JWT_KEY
+        if ('basicfrontendapp' == env('LASALLE_APP_NAME')) {
+            echo "\n\n";
+            $this->line('-----------------------------------------------------------------------');
+            $this->line('  Now setting your LASALLE_JWT_KEY environment variable');
+            $this->line('-----------------------------------------------------------------------');
+            echo "\n\n";
+            $this->comment(' ');
+            $lasalleJwtKey = Str::random(64);
+            $this->comment('Attempting to set LASALLE_JWT_KEY in your .env to "'.$lasalleJwtKey.'"...');
+            $this->writeEnvironmentFileWithNewKey('DummyJwtKey', $lasalleJwtKey, false);
+            $this->info("Attempt to modify your env's LASALLE_JWT_KEY to ".$lasalleJwtKey.' is finished!');
         }
         // END: SET THE PARAMS IN .ENV
         // -------------------------------------------------------------------------------------------------------------
-
-
 
         // -------------------------------------------------------------------------------------------------------------
         // START: DONE!
@@ -240,7 +239,13 @@ class LasalleinstallenvCommand extends CommonCommand
         $this->line('        Congratulations! You finished lslibrary:lasalleinstallenv!');
         $this->line('--------------------------------------------------------------------------');
 
-        if (env('LASALLE_APP_NAME') == 'adminbackendapp') {
+        if ('basicfrontendapp' == env('LASALLE_APP_NAME')) {
+            echo "\n\n";
+            $this->line('  Please run "php artisan lslibrary:lasalleinstallfrontendapp" to complete your installation');
+            $this->line('--------------------------------------------------------------------------');
+        }
+
+        if ('adminbackendapp' == env('LASALLE_APP_NAME')) {
             echo "\n\n";
             $this->line('  Please run "php artisan lslibrary:lasalleinstalladminapp" to complete your installation');
             $this->line('--------------------------------------------------------------------------');
@@ -250,7 +255,7 @@ class LasalleinstallenvCommand extends CommonCommand
     }
 
     /**
-     * Echo the final message
+     * Echo the final message.
      *
      * return void
      */
@@ -261,14 +266,12 @@ class LasalleinstallenvCommand extends CommonCommand
         $this->info('              ** lslibrary:lasalleinstallenv has finished **');
         $this->info('====================================================================');
         echo "\n\n";
-        return;
     }
 
     /**
-     * @param  text $patternToSearchFor         The text being searched
-     * @param  text $envFileDummyKey            The dummy key to be replaced in .env
-     * @param  bool $useQuotesInTheReplacement  Do you want to use quotes in the replacement string?
-     * @return void
+     * @param text $patternToSearchFor        The text being searched
+     * @param text $envFileDummyKey           The dummy key to be replaced in .env
+     * @param bool $useQuotesInTheReplacement Do you want to use quotes in the replacement string?
      */
     protected function writeEnvironmentFileWithNewKey($patternToSearchFor, $envFileDummyKey, $useQuotesInTheReplacement = true)
     {
@@ -276,7 +279,7 @@ class LasalleinstallenvCommand extends CommonCommand
 
         $pattern = $this->pattern($patternToSearchFor);
 
-        $replacement = $useQuotesInTheReplacement ? "'" . $envFileDummyKey . "'" : $envFileDummyKey;
+        $replacement = $useQuotesInTheReplacement ? "'".$envFileDummyKey."'" : $envFileDummyKey;
 
         $envFile = preg_replace($pattern, $replacement, $envFile);
 
@@ -289,16 +292,17 @@ class LasalleinstallenvCommand extends CommonCommand
      * The APP_URL *must* start with "http://" or "https://". However, if it does not, the APP_URL is returned,
      * just so something is returned.
      *
-     * @param  text   $appURL   The APP_URL
+     * @param text $appURL The APP_URL
+     *
      * @return string
      */
     protected function getLasalleAppDomainName($appURL)
     {
-        if (substr($appURL,0,7) == "http://") {
+        if ('http://' == substr($appURL, 0, 7)) {
             return substr($appURL, 7, strlen($appURL));
         }
 
-        if (substr($appURL,0,8) == "https://") {
+        if ('https://' == substr($appURL, 0, 8)) {
             return substr($appURL, 8, strlen($appURL));
         }
 
