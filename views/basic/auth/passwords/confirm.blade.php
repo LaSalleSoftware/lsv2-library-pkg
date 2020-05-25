49344464187e8106a7a9d35ff03cfc8fa856901f
Bob Bloom <bob.bloom@lasallesoftware.ca>
2020-05-25 09:53:18 -0400

    Fix layout path in password confirm view. #81

--- views/basic/auth/passwords/confirm.blade.php
+++ views/basic/auth/passwords/confirm.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('lasallesoftwarelibrary::basic.layouts.app')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">EDDIE!!!{{ __('Confirm Password') }}</div>
+                <div class="card-header">{{ __('Confirm Password') }}</div>
 
                 <div class="card-body">
                     {{ __('Please confirm your password before continuing.') }}
